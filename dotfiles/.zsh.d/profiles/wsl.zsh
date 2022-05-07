4520d369366bba45a9a172762f2d6a4c400545cf
ypc2e55orj <github@marukunaru.net>
2022-05-08 00:52:52 +0900

    rename fix_interop -> _rc_fix_interop

--- dotfiles/.zsh.d/profiles/wsl.zsh
+++ dotfiles/.zsh.d/profiles/wsl.zsh
@@ -1,12 +1,12 @@
 # wsl interop workaround
 # https://github.com/microsoft/WSL/issues/7174#issuecomment-940163080
-fix_interop() {
+_rc_fix_interop() {
     ps -o ppid= -p ${1:-$$} | read ppid
     [[ $(ps -o cmd= $ppid) = "/init" ]] && echo $ppid || fix_interop $ppid
 }
 
 if (uname -r | grep WSL > /dev/null);then
-    export WSL_INTEROP="/run/WSL/$(fix_interop)_interop"
+    export WSL_INTEROP="/run/WSL/$(_rc_fix_interop)_interop"
 
     export GTK_IM_MODULE=fcitx
     export QT_IM_MODULE=fcitx
