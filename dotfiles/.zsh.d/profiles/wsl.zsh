1e4f9bc100abcf23ba93f81d0239495de7b4eb91
ypc2e55orj <github@marukunaru.net>
2022-05-08 00:53:56 +0900

    fix broken _rc_fix_interop

--- dotfiles/.zsh.d/profiles/wsl.zsh
+++ dotfiles/.zsh.d/profiles/wsl.zsh
@@ -2,7 +2,7 @@
 # https://github.com/microsoft/WSL/issues/7174#issuecomment-940163080
 _rc_fix_interop() {
     ps -o ppid= -p ${1:-$$} | read ppid
-    [[ $(ps -o cmd= $ppid) = "/init" ]] && echo $ppid || fix_interop $ppid
+    [[ $(ps -o cmd= $ppid) = "/init" ]] && echo $ppid || _rc_fix_interop $ppid
 }
 
 if (uname -r | grep WSL > /dev/null);then
