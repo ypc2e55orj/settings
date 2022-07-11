87fac104a359046469dc2c0e57233747e965f98a
ypc2e55orj <github@marukunaru.net>
2022-07-11 17:48:22 +0900

    use npiperelay.exe

--- dotfiles/.zsh.d/profiles/wsl.zsh
+++ dotfiles/.zsh.d/profiles/wsl.zsh
@@ -15,7 +15,8 @@ if (uname -r | grep WSL > /dev/null);then
 
     (_rc_execute fcitx-autostart > /dev/null 2>&1 &)
 
-    [[ -e /tmp/windows-ssh-agent.sock ]] && export SSH_AUTH_SOCK=/tmp/windows-ssh-agent.sock
+    export SSH_AUTH_SOCK=/tmp/windows-ssh-agent-$RANDOM.sock
+    (socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork 2>&1 &)
 
     [[ $HOST = "" ]] && export GDK_SCALE=2 QT_SCALE_FACTOR=2
 fi
