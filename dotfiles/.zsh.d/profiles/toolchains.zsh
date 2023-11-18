da0c6afcf761768a1b6446329623e2ec5c243fcf
ypc2e55orj <ypc2e55orj@gmail.com>
2023-11-18 18:21:00 +0900

    使っていない設定を削除

--- dotfiles/.zsh.d/profiles/toolchains.zsh
+++ dotfiles/.zsh.d/profiles/toolchains.zsh
@@ -8,21 +8,9 @@ export VISUAL=vim
 # go
 _rc_path ~/.local/go/bin
 
-# haskell
-_rc_path ~/.cabal/bin
-_rc_path ~/.ghcup/bin
-
-_rc_path "$(_rc_execute stack path --compiler-bin)"
-
-# jvm
-_rc_source ~/.sdkman/bin/sdkman-init.sh
-
 # node
 export N_PREFIX=~/.n
 _rc_path $N_PREFIX/bin
-
-# ros2 galactic
-_rc_source /opt/ros/galactic/setup.zsh
 
 # esp-idf
 export IDF_PATH=/opt/esp-idf
@@ -35,14 +23,7 @@ _rc_path ~/.cargo/bin
 export PIPENV_VENV_IN_PROJECT=true
 export PYENV_PYTHON=$PYENV_ROOT/shims/python
 export PYENV_ROOT=~/.pyenv
-_rc_source ~/.rye/env
 
 _rc_path $PYENV_ROOT/bin
 _rc_path $PYENV_ROOT/shims
 
-# passerine
-_rc_path ~/.aspen/bin
-
-# php
-_rc_path ~/.phpenv/bin
-eval "$(_rc_execute phpenv init -)"
